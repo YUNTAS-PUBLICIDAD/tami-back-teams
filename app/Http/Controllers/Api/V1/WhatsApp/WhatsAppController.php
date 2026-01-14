e4f7fe2186534d61604b672ec159148b72c43b1e
darkMorgan <fabriziozevallos159@gmail.com>
2026-01-14 13:02:54 -0500

    cambios en wasap envio

--- app/Http/Controllers/Api/V1/WhatsApp/WhatsAppController.php
+++ app/Http/Controllers/Api/V1/WhatsApp/WhatsAppController.php
@@ -40,7 +40,7 @@ class WhatsAppController extends Controller
                 'description' => $producto->descripcion,
                 'phone'       => $request->phone,
                 'email'       => $request->email,
-                'imageData'   => $this->convertImageToBase64($imageUrl),
+                'imageData'   => $this->convertImageToBase64($defaultImageUrl),
             ]);
             $resultados['whatsapp'] = 'Mensaje de WhatsApp enviado correctamente ✅';
         } catch (\Throwable $e) {
